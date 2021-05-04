9ad3a4ae5303c59c5c6d0c090563adf7501f68cc
tejasbana <tejasbana@gmail.com>
2021-05-04 20:06:17 +0530

    basic

--- grid.py
+++ grid.py
@@ -5,10 +5,13 @@ root = Tk()
 # Define Label widget
 myLabel1 = Label(root, text="Hello World!")
 myLabel2 = Label(root, text="My name is Tejas Bana")
+emptyLabel = Label(root, text="            ")
 
-# Display using grid system
+# Display using grid system && matrix index are relative
 myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
+# Trick: for adding space between columns add empty label
+emptyLabel.grid(row=1,column=1)
+myLabel2.grid(row=1, column=5)
 
 
 root.mainloop()
