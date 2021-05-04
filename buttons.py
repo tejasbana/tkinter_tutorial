5c81975c3363e6e329cf8c879bfa0244fd078970
tejasbana <tejasbana@gmail.com>
2021-05-04 20:23:44 +0530

    button

--- buttons.py
+++ buttons.py
@@ -7,7 +7,9 @@ def gotClicked():
 	myLabel.pack()
 
 # Define button widget
-myButton = Button(root, text="Click Me!", padx=50, command=gotClicked)
+myButton = Button(root, text="Click Me!", padx=50,
+						fg="red", bg="#000000", 
+				  		command=gotClicked)
 myButton.pack()
 
 root.mainloop()
