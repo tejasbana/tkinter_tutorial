6a89b5fbf4f8f0a2b89a2ec7f66b36d32177bffc
tejasbana <tejasbana@gmail.com>
2021-05-04 20:02:02 +0530

    basic

---
+++ HelloWorkd.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+# Define Label widget
+myLabel = Label(root, text="Hello World!")
+
+# Display widget
+myLabel.pack() 			# pack doesn't give alot of control, keep the wigdet static
+
+
+root.mainloop()
---
+++ grid.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
+# Define Label widget
+myLabel1 = Label(root, text="Hello World!")
+myLabel2 = Label(root, text="My name is Tejas Bana")
+
+# Display using grid system
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+
+
+root.mainloop()
