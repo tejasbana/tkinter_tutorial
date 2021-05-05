bad9e336123ef6bdcec6de4308cbc7050b29624e
tejasbana <tejasbana@gmail.com>
2021-05-05 11:47:35 +0530

    Entry

--- entry.py
+++ entry.py
@@ -5,11 +5,14 @@ root = Tk()
 # Input entry box
 e = Entry(root, width=50, borderwidth=5, bg="black", fg="white")
 e.pack()
+# Default value for entry box
+e.insert(0, "Enter Your Name:  ")
 # e.get() to get the content of entry
 
 # Get the content of input box after button has been clicked
 def gotClicked():
-	myLabel = Label(root, text=e.get())
+	outpus_string = "Hello " + e.get()
+	myLabel = Label(root, text=outpus_string)
 	myLabel.pack()
 
 # Define button widget
