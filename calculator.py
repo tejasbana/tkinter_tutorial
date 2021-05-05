ded76a7a5d9a41227eeef0f4c1d3b7b59f905bce
tejasbana <tejasbana@gmail.com>
2021-05-05 13:20:45 +0530

    Add Addition

--- calculator.py
+++ calculator.py
@@ -18,6 +18,17 @@ def button_click(number):
 def button_clear():
 	e.delete(0,END)
 
+def button_add():
+	first_number = e.get()
+	global f_num 
+	f_num = int(first_number)
+	e.delete(0, END)
+
+def button_equal():
+	num_sum = f_num + int(e.get())
+	e.delete(0, END)
+	e.insert(0, num_sum)
+
 # Define button widget
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
 button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
@@ -29,8 +40,8 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-button_add = Button(root, text="+", padx=39, pady=20, command=lambda: button_click())
-button_equal = Button(root, text="=", padx=91, pady=20, command=lambda: button_click())
+button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
+button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
 button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
 
 
