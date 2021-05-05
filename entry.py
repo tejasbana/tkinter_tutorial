6db9ae817fe1b62837f8451a9271345748216488
tejasbana <tejasbana@gmail.com>
2021-05-05 11:41:57 +0530

    get entry

--- entry.py
+++ entry.py
@@ -2,11 +2,14 @@ from tkinter import *
 
 root = Tk()
 
-e = Entry(root, width=50, bg="black", fg="white")
+# Input entry box
+e = Entry(root, width=50, borderwidth=5, bg="black", fg="white")
 e.pack()
+# e.get() to get the content of entry
 
+# Get the content of input box after button has been clicked
 def gotClicked():
-	myLabel = Label(root, text="Just got clicked!")
+	myLabel = Label(root, text=e.get())
 	myLabel.pack()
 
 # Define button widget
