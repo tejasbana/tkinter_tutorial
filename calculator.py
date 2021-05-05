9b817182f1cb7f3c7ee2d8cb685890466ed33da9
tejasbana <tejasbana@gmail.com>
2021-05-05 12:58:44 +0530

    calculator

--- calculator.py
+++ calculator.py
@@ -23,6 +23,10 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
 button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
 button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
 button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
+button_equal = Button(root, text="=", padx=91, pady=20, command=button_add)
+button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_add)
+
 
 # Put buttons on the screen
 button_1.grid(row=3, column=0)
@@ -38,6 +42,9 @@ button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
+button_clear.grid(row=4, column=1, columnspan=2)
+button_add.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
 
 
 # myButton = Button(root, text="Click Me!", padx=50,
