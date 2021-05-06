6038bb50f14569a6d6b145a91ffb8ef9786cef02
tejasbana <tejasbana@gmail.com>
2021-05-06 19:13:28 +0530

    Images, Icons and Exit buttons

--- images.py
+++ images.py
@@ -1,10 +1,23 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title("Images, Exit buttons")
 
 # Load Icons 
-# root.iconbitmap('icon/chameleon.ico')
+# root.iconbitmap('./icon/chameleon.ico')
+
+
+# Load and Display Image: 3 steps
+my_img = ImageTk.PhotoImage( Image.open("./icon/chameleon.png"))
+my_label = Label(image=my_img)
+my_label.pack()
+
+
+
+
+
+
 
 # Exit Buttons
 button_quit = Button(root, text="Exit Program", command=root.quit)
