c5bb76f75852d4e8678a45f856956ee4df67e8b6
tejasbana <tejasbana@gmail.com>
2021-05-10 14:29:55 +0530

    Display Logo

--- Apps/pdf_reader_app.py
+++ Apps/pdf_reader_app.py
@@ -6,8 +6,11 @@ root = tk.Tk()
 canvas = tk.Canvas(root, width=600, height=300)
 canvas.grid(columnspan=3) # Divide canvas into equal columnspan
 
-# Logo
+# Read Logo & Display
 logo = Image.open("./logo.png")
 logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
 
 root.mainloop()
