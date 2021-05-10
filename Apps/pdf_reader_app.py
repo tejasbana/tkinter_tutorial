6389d29f1c0f16d664fbd6b5fe23e7f9f40ea360
tejasbana <tejasbana@gmail.com>
2021-05-10 14:27:11 +0530

    Read logo

--- Apps/pdf_reader_app.py
+++ Apps/pdf_reader_app.py
@@ -2,7 +2,12 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 root = tk.Tk()
+# Create a canvas of specific size
 canvas = tk.Canvas(root, width=600, height=300)
-canvas.grid(columnspan=3)
+canvas.grid(columnspan=3) # Divide canvas into equal columnspan
+
+# Logo
+logo = Image.open("./logo.png")
+logo = ImageTk.PhotoImage(logo)
 
 root.mainloop()
